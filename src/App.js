fa42388a89ae7ecd70fa2195207d9d1c7b03097d
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-04-02 11:31:37 -0600

    Front end skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
+import Finder from './components/Finder'
+import Pokedex from './components/Pokedex'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      caughtPokemon: [],
+    }
+    this.catchPokemon = this.catchPokemon.bind(this)
+    this.saveName = this.saveName.bind(this)
+    this.releasePokemon = this.releasePokemon.bind(this)
+  }
+
+  componentDidMount() {}
+
+  catchPokemon(pokemon) {}
+
+  saveName(id, newName) {}
+
+  releasePokemon(id) {}
+
   render() {
-    return <div className="App"></div>
+    return (
+      <div className="App">
+        App.js
+        <Header />
+        <Finder catchPokemon={this.catchPokemon} />
+        <Pokedex
+          caughtPokemon={this.state.caughtPokemon}
+          saveName={this.saveName}
+          releasePokemon={this.releasePokemon}
+        />
+      </div>
+    )
   }
 }
 
